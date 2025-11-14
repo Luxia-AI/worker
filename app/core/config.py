0ed72c5e74cca8b3b6afbec0bebf3fac9a62bac5
nivindulakshitha <nivindulakshitha@gmail.com>
2025-11-15 05:04:18 +0530

    fix: update environment variable for Pinecone API key and adjust config settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,12 @@
 from typing import Optional
 
-from pydantic import ConfigDict
+from pydantic import ConfigDict, Field
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    pinecone_api_key: str = None
-    pinecone_index_name: Optional[str] = None
-    pinecone_org_name: Optional[str] = None
-    pinecone_org_id: Optional[str] = None
-    pinecone_project_name: Optional[str] = None
-    pinecone_project_id: Optional[str] = None
+    pinecone_api_key: Optional[str] = Field(default=None)
+    pinecone_index_name: Optional[str] = Field(default=None)
 
     model_config = ConfigDict(env_file=".env", extra="ignore")
 
