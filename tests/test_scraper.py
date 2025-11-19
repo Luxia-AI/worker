f1e7403345c17d72018d79978419debd9e8e5dc3
nivindulakshitha <nivindulakshitha@gmail.com>
2025-11-19 11:46:34 +0530

    Fix test scraper URL for accurate data retrieval

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -6,6 +6,6 @@ from app.services.corrective.scraper import Scraper
 @pytest.mark.asyncio
 async def test_scraper():
     s = Scraper()
-    urls = ["https://www.cdc.gov/coronavirus/2019-ncov/vaccines/effectiveness.html"]
+    urls = ["https://cdc.gov/healthy-weight-growth/about/tips-for-balancing-food-activity.html"]
     results = await s.scrape_all(urls)
     print(results)
